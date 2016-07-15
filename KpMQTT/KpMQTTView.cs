73d4b88ddb8f14df3e8e6c7e12979f41e42077e6
bersim <vvk.kos@gmail.com>
2016-07-15 23:17:32 +0500

    Загрузка проекта KpMQTT

---
+++ KpMQTT/KpMQTTView.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Scada.Comm.Devices
+{
+	public class KpMQTTView : KPView
+	{
+		public KpMQTTView () : this(0)
+		{
+		}
+
+		public KpMQTTView(int number)
+			: base(number)
+		{
+		}
+
+		public override string KPDescr
+		{
+			get
+			{
+				return Localization.UseRussian ? 
+					"Библиотека КП для тестирования." : 
+					"Device library for testing.";
+			}
+		}
+
+	}
+}
+
